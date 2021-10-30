f022aa55966cd8022ed8b62a64ad02e4579b7adb
Your Name <you@example.com>
2021-10-30 19:52:58 -0300

    criando user

--- src/main/java/com/spring/Helpdesk/controllers/AuthController.java
+++ src/main/java/com/spring/Helpdesk/controllers/AuthController.java
@@ -1,14 +1,28 @@
 package com.spring.Helpdesk.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.spring.Helpdesk.models.User;
+import com.spring.Helpdesk.services.UserService;
 
 @Controller
 public class AuthController {
+	
+	@Autowired
+	private UserService userService;
+	
+	public AuthController(UserService userService) {
+		this.userService = userService;
+	}
 
 	@GetMapping("/login")
 	public String login(Model model) {
@@ -21,5 +35,14 @@ public class AuthController {
 		return "auth/registration";
 	}
 	
+	@PostMapping("/registration")
+	public String save(@Valid @ModelAttribute("user") User user, BindingResult bindinResult, Model model) {
+		if(bindinResult.hasErrors()) {
+			return "auth/registration";
+		}
+		
+		this.userService.create(user);
+		return "redirect:/login";
+	}
 	
 }
